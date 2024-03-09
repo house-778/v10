69a16c0f4a971308f9c0f88bb12105961451cc8a
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-09 15:38:25 +0000

    run faster

--- buttonan.js
+++ buttonan.js
@@ -5,8 +5,4 @@ document.addEventListener('DOMContentLoaded', function () {
       style.textContent = `.game_button:nth-child(${i}) { animation: fadeInUp 1s ease-in-out ${0.1 * i}s forwards; }`;
       document.head.appendChild(style);
   }
-  const buttons = document.querySelectorAll('.game_button');
-  buttons.forEach((button, index) => {
-      button.style.animationDelay = `${0.02 * (index + 1)}s`;
-  });
 });
