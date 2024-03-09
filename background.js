8c6766a87ef8597a1fa821fcdb9f6b50e27014b2
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-09 17:05:49 +0000

    anermated backgrounded

---
+++ background.js
@@ -0,0 +1,66 @@
+function gra(){
+    var bodyElement = document.body;
+    var grab = document.getElementById('gra');
+    var darkb = document.getElementById('dark');
+    var lightb = document.getElementById('light');
+    grab.style.display = 'none';
+    darkb.style.display = 'block';
+    lightb.style.display = 'none';
+    bodyElement.classList = [];
+    bodyElement.classList.add("gra");
+    window.sessionStorage.background = "gra";
+}
+function dark(){
+    var bodyElement = document.body;
+    var grab = document.getElementById('gra');
+    var darkb = document.getElementById('dark');
+    var lightb = document.getElementById('light');
+    grab.style.display = 'none';
+    darkb.style.display = 'none';
+    lightb.style.display = 'block';
+    bodyElement.classList = [];
+    bodyElement.classList.add("dark");
+    window.sessionStorage.background = "dark";
+}
+function light(){
+    var bodyElement = document.body;
+    var grab = document.getElementById('gra');
+    var darkb = document.getElementById('dark');
+    var lightb = document.getElementById('light');
+    grab.style.display = 'block';
+    darkb.style.display = 'none';
+    lightb.style.display = 'none';
+    bodyElement.classList = [];
+    bodyElement.classList.add("light");
+    window.sessionStorage.background = "light";
+}
+var bodyElement = document.body;
+if (bodyElement.classList.length === 0) {
+    var fullPath = window.location.pathname;
+    var fileName = fullPath.split('/').pop();
+    if(fileName == "game.html"){
+        var bodyElement = document.body;
+        var grab = document.getElementById('gra');
+        var darkb = document.getElementById('dark');
+        var lightb = document.getElementById('light');
+        grab.style.display = 'none';
+        darkb.style.display = 'block';
+        lightb.style.display = 'none';
+    }
+    bodyElement.classList = [];
+    bodyElement.classList.add("gra");
+    window.sessionStorage.background = "gra";
+}
+if (window.sessionStorage.background == "gra"){
+    var bodyElement = document.body;
+    bodyElement.classList = [];
+    bodyElement.classList.add("gra");
+}else if (window.sessionStorage.background == "light"){
+    var bodyElement = document.body;
+    bodyElement.classList = [];
+    bodyElement.classList.add("light");
+}else if (window.sessionStorage.background == "dark"){
+    var bodyElement = document.body;
+    bodyElement.classList = [];
+    bodyElement.classList.add("dark");
+}
--- buttonan.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const buttonCount = 500;
-  for (let i = 1; i <= buttonCount; i++) {
-      const style = document.createElement('style');
-      style.textContent = `.game_button:nth-child(${i}) { animation: fadeInUp 1s ease-in-out ${0.1 * i}s forwards; }`;
-      document.head.appendChild(style);
-  }
-});
