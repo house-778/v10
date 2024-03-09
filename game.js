903e30be0f67cba482210b2b39de1dc1ddf75ef6
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-09 17:58:05 +0000

    fix hop

--- game.js
+++ game.js
@@ -22,22 +22,13 @@ function searchGames() {
 }
 
 window.addEventListener('load', () => sortButtonsAlphabetically());
-function getCleanFileName(page) {
-  var pathArray = page.split('/');
-  var fileName = pathArray.pop();
-  fileName = fileName.replace('_new', '');
-  fileName = fileName.replace(/_/g, ' ');
-  fileName = fileName.replace(/-/g, ' ');
-  fileName = fileName.replace(/.html/g, '');
-  return fileName;
-}
-function goToPage(page) {
-  window.location.href = page;
-}
+
+
+
 var visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
 var visitedPagesContainer = document.getElementById('visitedPages');
 if (visitedPages.length === 0) {
-  visitedPagesContainer.innerHTML += '<p style = "color: #fff;">You have not played any games on this conputer yet.</p>';
+  visitedPagesContainer.innerHTML += '<p style="color: #fff;">You have not played any games on this computer yet.</p>';
 } else {
   visitedPages.forEach(function (page) {
   var cleanFileName = getCleanFileName(page);
