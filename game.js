10b30888f5b60a30521c1e64db7d92e6e698b913
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-09 12:27:55 +0000

    Update game.js

--- game.js
+++ game.js
@@ -28,21 +28,22 @@ function sortButtonsAlphabetically() {
 }
 
 function searchGames() {
-  var input, filter, buttons, button, i, txtValue;
-  input = document.getElementById("searchInput");
-  filter = input.value.toUpperCase();
-  buttons = document.getElementsByClassName("game_button");
+    var input, filter, buttons, button, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    buttons = document.getElementsByClassName("game_button");
 
-  for (i = 0; i < buttons.length; i++) {
-      button = buttons[i];
-      txtValue = button.innerText || button.textContent;
+    for (i = 0; i < buttons.length; i++) {
+        button = buttons[i];
+        txtValue = button.innerText || button.textContent;
 
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          button.style.display = "";
-      } else {
-          button.style.display = "none";
-      }
-  }
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            button.style.display = "none";
+            button.style.display = "";
+        } else {
+            button.style.display = "none";
+        }
+    }
   sortButtonsAlphabetically();
 }
 sortButtonsAlphabetically();
