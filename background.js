8aceb71fb9438070e7faf11996ad9e64e3fd492d
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-09 17:15:15 +0000

    bug fix

--- background.js
+++ background.js
@@ -34,22 +34,6 @@ function light(){
     bodyElement.classList.add("light");
     window.sessionStorage.background = "light";
 }
-var bodyElement = document.body;
-if (window.sessionStorage.background == "") {
-    var fullPath = window.location.pathname;
-    var fileName = fullPath.split('/').pop();
-    if(fileName == "game.html"){
-        var bodyElement = document.body;
-        var grab = document.getElementById('gra');
-        var darkb = document.getElementById('dark');
-        var lightb = document.getElementById('light');
-        grab.style.display = 'none';
-        darkb.style.display = 'block';
-        lightb.style.display = 'none';
-    }
-    bodyElement.classList.add("gra");
-    window.sessionStorage.background = "gra";
-}
 if (window.sessionStorage.background == "gra"){
     var bodyElement = document.body;
     bodyElement.classList = [];
@@ -62,4 +46,19 @@ if (window.sessionStorage.background == "gra"){
     var bodyElement = document.body;
     bodyElement.classList = [];
     bodyElement.classList.add("dark");
+}else{
+    var fullPath = window.location.pathname;
+    var fileName = fullPath.split('/').pop();
+    if(fileName == "game.html"){
+        var bodyElement = document.body;
+        var grab = document.getElementById('gra');
+        var darkb = document.getElementById('dark');
+        var lightb = document.getElementById('light');
+        grab.style.display = 'none';
+        darkb.style.display = 'block';
+        lightb.style.display = 'none';
+    }
+    bodyElement.classList = [];
+    bodyElement.classList.add("gra");
+    window.sessionStorage.background = "gra";
 }
